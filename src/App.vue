56358e4e62b3b6d164fbca1d1f43c903fa7c5653
Christopher Hill <cjameshill@gmail.com>
2022-08-18 10:04:07 +0000

    Replicated css styles to button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
-import { onMounted, ref } from "vue"
+import { onMounted, reactive } from "vue"
+import { useEventListener } from '@vueuse/core'
 import { useButtonsComposable } from "./composables/buttonsComposable";
-const { buttonColor, buttonText, logo } = useButtonsComposable();
+const { buttonColor, buttonText, logo, setContent } = useButtonsComposable();
+let inputData = reactive({
+    data: {}
+})
+const buttonClick = () => {
+    window.open("https://www.apple.com/",
+        "cpLoginPopup",
+        "status=1, height=800, width=500, toolbar=0,resizable=0");
+}
+useEventListener(window, 'message', (event) => {
+    console.log('event: ', event.origin)
+})
+
 onMounted(() => {
     const button = document.getElementById("button");
-    console.log(button.dataset["cpUserEmail"])
+    for (let i in button.dataset) {
+        inputData.data[i] = button.dataset[i];
+    }
+
 })
 </script>
 
 <template>
-    <button id="primaryButton"
+    <button id="primaryButton" @click="buttonClick"
         class="flex flex-row items-center py-1 pl-2 pr-3 w-80 rounded-xl text-white text-[17px] border-2 border-transparent hover:bg-opacity-80 focus:border-gray-400"
         :class="buttonColor">
         <img class="flex-none w-6 h-6 mr-3" :src="logo" />
--- src/composables/buttonsComposable.js
+++ src/composables/buttonsComposable.js
@@ -2,6 +2,47 @@ import { ref } from "vue"
 const buttonText = ref("Start with CrewPass")
 const logo = "https://storage.googleapis.com/crewpass-production-loginbutton/cp-icon.png";
 const buttonColor = ref("bg-[#2B3D4B]")
+
+const content = {
+    buttonText: "Approve with CrewPass",
+    pleaseWait: "Please wait...",
+    statuses: {
+        "not-checked": {
+            buttonText: "Approve With CrewPass",
+            backgroundColor: "#2B3D4B"
+        },
+        loading: {
+            buttonText: "Please Wait..",
+            backgroundColor: "#2B3D4B"
+        },
+        pending: {
+            buttonText: "Pending",
+            backgroundColor: "#F39200"
+        },
+        approved: {
+            buttonText: "Approved",
+            backgroundColor: "#3AAA35"
+        },
+        verified: {
+            buttonText: "Approved",
+            backgroundColor: "#3AAA35"
+        },
+        declined: {
+            buttonText: "Declined",
+            backgroundColor: "#E6332A"
+        },
+        unchecked: {
+            buttonText: "Unchecked",
+            backgroundColor: "#878787"
+        },
+    },
+}
+
+const setContent = (status) => {
+    buttonColor.value = content.statuses[status || "not-checked"]?.backgroundColor;
+    buttonText.value = content.statuses[status || "not-checked"]?.buttonText;
+}
+
 export function useButtonsComposable() {
-    return { buttonColor, buttonText, logo }
+    return { buttonColor, buttonText, logo, setContent }
 }
