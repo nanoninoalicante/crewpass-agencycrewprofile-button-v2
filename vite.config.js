eea97a387255dd72c9137762ad4a3b05afb732c4
Christopher Hill <cjameshill@gmail.com>
2022-08-18 10:13:31 +0000

    Added cloudbuild

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,14 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  rollupPluginVueOptions: {
-    cssModulesOptions: {
-      generateScopedName: "[hash:base64:8]",
-    },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   },
   resolve: {
     alias: {
