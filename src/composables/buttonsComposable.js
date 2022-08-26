f9c86c5e174f931668cc4b364f5c193889729596
Christopher Hill <cjameshill@gmail.com>
2022-08-26 11:47:12 +0000

    Added cp-partner data param

--- src/composables/buttonsComposable.js
+++ src/composables/buttonsComposable.js
@@ -1,80 +1,95 @@
-import { ref, computed, reactive } from "vue"
+import { ref, computed, reactive } from "vue";
 import { useGeneralComposable } from "./generalComposable";
-const { remapData } = useGeneralComposable()
-const buttonText = ref("Approve With CrewPass")
-const logo = "https://storage.googleapis.com/crewpass-production-loginbutton/cp-icon.png";
+const { remapData } = useGeneralComposable();
+const buttonText = ref("Approve With CrewPass");
+const logo =
+  "https://storage.googleapis.com/crewpass-production-loginbutton/cp-icon.png";
 const crewStatus = ref("not-checked");
 const content = {
-    buttonText: "Approve with CrewPass",
-    pleaseWait: "Please wait...",
-    statuses: {
-        "not-checked": {
-            buttonText: "Approve With CrewPass"
-        },
-        loading: {
-            buttonText: "Please Wait.."
-        },
-        pending: {
-            buttonText: "Pending"
-        },
-        approved: {
-            buttonText: "Approved"
-        },
-        verified: {
-            buttonText: "Approved"
-        },
-        declined: {
-            buttonText: "Declined"
-        },
-        unchecked: {
-            buttonText: "Unchecked"
-        },
+  buttonText: "Approve with CrewPass",
+  pleaseWait: "Please wait...",
+  statuses: {
+    "not-checked": {
+      buttonText: "Approve With CrewPass",
     },
-}
+    loading: {
+      buttonText: "Please Wait..",
+    },
+    pending: {
+      buttonText: "Pending",
+    },
+    approved: {
+      buttonText: "Approved",
+    },
+    verified: {
+      buttonText: "Approved",
+    },
+    declined: {
+      buttonText: "Declined",
+    },
+    unchecked: {
+      buttonText: "Unchecked",
+    },
+  },
+};
 
 const setContent = (status) => {
-    crewStatus.value = status;
-    buttonText.value = content.statuses[status || "not-checked"]?.buttonText;
-}
+  crewStatus.value = status;
+  buttonText.value = content.statuses[status || "not-checked"]?.buttonText;
+};
 
 let inputData = reactive({
-    data: {}
-})
+  data: {},
+});
 
 const loading = ref(false);
 
 const sanitizedParams = computed(() => {
-    const keys = {
-        "cpUserId": 'id',
-        "cpUserEmail": 'email',
-        "cpFirstName": 'fname',
-        "cpLastName": 'lname',
-        "cpNationality": 'nationality',
-        "cpDob": 'birthday',
-        "cpGender": 'gender',
-        "cpCountryCode": 'countrycode',
-        "cpPhone": 'phoneno',
-        "cpStreetAddress": 'address',
-        "cpCity": 'city',
-        "cpState": 'state',
-        "cpCountry": 'country',
-    }
-    return remapData(keys, inputData.data);
-})
+  const keys = {
+    cpPartner: "partner",
+    cpUserId: "id",
+    cpUserEmail: "email",
+    cpFirstName: "fname",
+    cpLastName: "lname",
+    cpNationality: "nationality",
+    cpDob: "birthday",
+    cpGender: "gender",
+    cpCountryCode: "countrycode",
+    cpPhone: "phoneno",
+    cpStreetAddress: "address",
+    cpCity: "city",
+    cpState: "state",
+    cpCountry: "country",
+  };
+  return remapData(keys, inputData.data);
+});
 const queryParams = computed(() => {
-    const params = new URLSearchParams(sanitizedParams.value);
-    return params.toString();
-})
+  const params = new URLSearchParams(sanitizedParams.value);
+  return params.toString();
+});
 const popupUrl = computed(() => {
-    return `https://verify-dev.crewpass.co.uk?${queryParams.value}`
-})
+  return `https://verify-dev.crewpass.co.uk?${queryParams.value}`;
+});
 const buttonClick = () => {
-    setContent("pending");
-    window.open(popupUrl.value,
-        "cpAgencyCrewLoginPopup",
-        "status=1, height=800, width=500, toolbar=0,resizable=0");
-}
+  setContent("pending");
+  window.open(
+    popupUrl.value,
+    "cpAgencyCrewLoginPopup",
+    "status=1, height=800, width=500, toolbar=0,resizable=0"
+  );
+};
 
 export function useButtonsComposable() {
-    return { crewStatus, buttonText, logo, popupUrl, buttonClick, setContent, queryParams, sanitizedParams, inputData, loading }
+  return {
+    crewStatus,
+    buttonText,
+    logo,
+    popupUrl,
+    buttonClick,
+    setContent,
+    queryParams,
+    sanitizedParams,
+    inputData,
+    loading,
+  };
 }
