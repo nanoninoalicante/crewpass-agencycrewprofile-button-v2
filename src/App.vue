75676eed435a5e52ae9ecdb6f6993f6f0b209e9a
Christopher Hill <cjameshill@gmail.com>
2022-09-01 15:03:51 +0000

    Added function to set response in storage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <script setup>
-import { onMounted } from "vue"
+import { onMounted, ref } from "vue"
 import { useEventListener } from '@vueuse/core'
 import { useButtonsComposable } from "./composables/buttonsComposable";
 import SpinnerIcon from "./components/SpinnerIcon.vue";
-const { crewStatus, buttonText, logo, buttonClick, inputData, loading, queryParams } = useButtonsComposable();
-// useEventListener(window, 'message', (event) => {
-//     console.log('event: ', event.origin)
-// })
+const { buttonText, logo, buttonClick, inputData, loading, setContent, popupOrigin, crewUserData, setMessageResponse, setButtonData } = useButtonsComposable();
+
+const messages = ref([]);
+
+useEventListener(window, 'message', (message) => {
+    console.log('message: ', message.origin)
+    if (message.origin === popupOrigin.value) {
+        console.log("message: ", message);
+        messages.value.push(message.data);
+        setMessageResponse(message.data);
+    }
+})
 
 onMounted(() => {
     const button = document.getElementById("cp-agency-crew-profile-button");
-    for (let i in button.dataset) {
-        inputData.data[i] = button.dataset[i];
+    setButtonData(button.dataset);
+    console.log("crewUserData.value: ", crewUserData.value?.status)
+    if (crewUserData.value && crewUserData.value?.status) {
+        setContent(crewUserData.value?.status?.toLowerCase())
     }
-
 })
 </script>
 
 <template>
     <button id="primaryButton" @click="buttonClick"
-        class="inline-flex flex-row items-center py-1 pl-2 pr-3 w-80 rounded-xl text-white text-[17px] border-2 border-transparent hover:bg-opacity-80 focus:border-gray-400"
-        :class="crewStatus">
+        class="inline-flex flex-row items-center py-1 pl-2 pr-3 w-80 rounded-xl text-white text-[17px] border-2 border-transparent hover:bg-gray-500 focus:border-gray-400"
+        :class="crewUserData.status">
         <img class="flex-none w-6 h-6 mr-3" :src="logo" />
         <div v-if="loading" class="flex-none">
             <SpinnerIcon class="w-5 h-5 fill-white animate-spin"></SpinnerIcon>
@@ -31,6 +40,7 @@ onMounted(() => {
             }}
         </div>
     </button>
+    <pre>{{ inputData.data }}</pre>
 </template>
 <style scoped>
 @import "./index.css";
--- src/composables/buttonsComposable.js
+++ src/composables/buttonsComposable.js
@@ -1,99 +1,143 @@
-import { ref, computed, reactive } from "vue";
+import { ref, computed, reactive, watch, onMounted } from "vue";
 import { useGeneralComposable } from "./generalComposable";
+import { useStorage } from '@vueuse/core'
+
 const { remapData } = useGeneralComposable();
-const buttonText = ref("Approve With CrewPass");
 const logo =
-  "https://storage.googleapis.com/crewpass-production-loginbutton/cp-icon.png";
-const crewStatus = ref("not-checked");
+	"https://storage.googleapis.com/crewpass-production-loginbutton/cp-icon.png";
+const buttonText = ref("Approve With CrewPass");
+const crewUserData = useStorage(`cp-crew-user`, { status: "not-checked"})
 const content = {
-  buttonText: "Approve with CrewPass",
-  pleaseWait: "Please wait...",
-  statuses: {
-    "not-checked": {
-      buttonText: "Approve With CrewPass",
-    },
-    loading: {
-      buttonText: "Please Wait..",
-    },
-    pending: {
-      buttonText: "Pending",
-    },
-    approved: {
-      buttonText: "Approved",
-    },
-    verified: {
-      buttonText: "Approved",
-    },
-    declined: {
-      buttonText: "Declined",
-    },
-    unchecked: {
-      buttonText: "Unchecked",
-    },
-  },
+	buttonText: "Approve with CrewPass",
+	pleaseWait: "Please wait...",
+	statuses: {
+		"not-checked": {
+			buttonText: "Approve With CrewPass",
+		},
+		loading: {
+			buttonText: "Please Wait..",
+		},
+		pending: {
+			buttonText: "Pending",
+		},
+		approved: {
+			buttonText: "Approved",
+		},
+		verified: {
+			buttonText: "Approved",
+		},
+		declined: {
+			buttonText: "Declined",
+		},
+		unchecked: {
+			buttonText: "Unchecked",
+		},
+	},
 };
 
 const setContent = (status) => {
-  crewStatus.value = status;
-  buttonText.value = content.statuses[status || "not-checked"]?.buttonText;
+	crewUserData.value.status = status;
+	buttonText.value = content.statuses[status || "not-checked"]?.buttonText;
 };
 
 let inputData = reactive({
-  data: {},
+	data: {},
 });
+
 
 const loading = ref(false);
 
 const sanitizedParams = computed(() => {
-  const keys = {
-    cpPartner: "partner",
-    cpUserId: "id",
-    cpUserEmail: "email",
-    cpFirstName: "fname",
-    cpLastName: "lname",
-    cpNationality: "nationality",
-    cpDob: "birthday",
-    cpGender: "gender",
-    cpCountryCode: "countrycode",
-    cpPhone: "phoneno",
-    cpStreetAddress: "address",
-    cpCity: "city",
-    cpState: "state",
-    cpCountry: "country",
-  };
-  let params = remapData(keys, inputData.data);
-  if (!params.partner) {
-    params.partner = "yotspot";
-  }
-  return params;
+	const keys = {
+		cpPartner: "partner",
+		cpUserId: "id",
+		cpUserEmail: "email",
+		cpFirstName: "fname",
+		cpLastName: "lname",
+		cpNationality: "nationality",
+		cpDob: "birthday",
+		cpGender: "gender",
+		cpCountryCode: "countrycode",
+		cpPhone: "phoneno",
+		cpStreetAddress: "address",
+		cpCity: "city",
+		cpState: "state",
+		cpCountry: "country",
+	};
+	let params = remapData(keys, inputData.data);
+	if (!params.partner) {
+		params.partner = "yotspot";
+	}
+	return params;
 });
 const queryParams = computed(() => {
-  const params = new URLSearchParams(sanitizedParams.value);
-  return params.toString();
+	const params = new URLSearchParams(sanitizedParams.value);
+	return params.toString();
 });
 const popupUrl = computed(() => {
-  return `https://verify-dev.crewpass.co.uk?${queryParams.value}`;
+	return `https://crewpass-testing-web.netlify.app/crew-messages?${queryParams.value}`;
 });
+const origin = window.location.origin;
+const popupFullUrlObject = computed(() => {
+	if (!popupUrl.value) return ""
+	const url = new URL(popupUrl.value);
+	url.searchParams.append("origin", origin);
+	return url;
+})
+const popupFullUrl = computed(() => {
+	if (!popupFullUrlObject.value) return ""
+	return popupFullUrlObject.value?.toString();
+})
+const popupOrigin = computed(() => {
+	if (!popupFullUrlObject.value || !popupFullUrlObject.value) return ""
+	return popupFullUrlObject.value?.origin;
+})
 const buttonClick = () => {
-  setContent("pending");
-  window.open(
-    popupUrl.value,
-    "cpAgencyCrewLoginPopup",
-    "status=1, height=800, width=500, toolbar=0,resizable=0"
-  );
+
+	window.open(
+		popupFullUrl.value,
+		"cpAgencyCrewLoginPopup",
+		"status=1, height=800, width=500, toolbar=0,resizable=0"
+	);
 };
 
+watch(crewUserData, (newValue) => {
+	console.log("crew user data updated: ", newValue)
+	if (newValue.status) {
+		setContent(newValue.status?.toLowerCase())
+	}
+})
+
+const setButtonData = (dataset) => {
+	for (let i in dataset) {
+		inputData.data[i] = dataset[i];
+	}
+
+}
+
+
+const setMessageResponse = (data) => {
+	crewUserData.value = {};
+	for (const item in data) {
+		crewUserData.value[item] = data[item];
+	}
+
+}
+
 export function useButtonsComposable() {
-  return {
-    crewStatus,
-    buttonText,
-    logo,
-    popupUrl,
-    buttonClick,
-    setContent,
-    queryParams,
-    sanitizedParams,
-    inputData,
-    loading,
-  };
+	return {
+		buttonText,
+		logo,
+		popupUrl,
+		buttonClick,
+		setContent,
+		queryParams,
+		sanitizedParams,
+		inputData,
+		loading,
+		popupOrigin,
+		setMessageResponse,
+		setButtonData,
+		crewUserData
+	};
 }
